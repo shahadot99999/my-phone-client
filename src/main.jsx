dd2f9038788ca45e1bab3609bd862a4a9f170726
shahadot99999 <shahadot99999@gmail.com>
2025-09-09 21:19:05 +0600

    some components and link connected with server computer

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Phones from './components/Phones.jsx';
+import Main from './components/Main.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    
+    children: [
+      {
+        path: "/phones",
+        element: <Phones />,
+        loader: ()=> fetch('http://localhost:3000/phones')
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
